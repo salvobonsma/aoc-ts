18db4b65741a859532eaf0f54b88523527c24ab5
ampersanded <salvobonsma@gmail.com>
2024-11-30 21:12:37 -0800

    day 1

--- day-1/a.ts
+++ day-1/a.ts
@@ -3,7 +3,17 @@ import { runSolution } from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
   console.log(data);
-  return 0;
+
+  const a = data
+      .map(value => +value.split("  ")[0])
+      .sort((a1, b1) => a1 - b1);
+  const b = data
+      .map(value => +value.split("  ")[1])
+      .sort((a1, b1) => a1 - b1);
+
+  return  a
+      .map((value, index) => Math.abs(value - b[index]))
+      .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -3,7 +3,12 @@ import { runSolution } from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day1b(data: string[]) {
   console.log(data);
-  return 0;
+    const a = data.map(value => +value.split("  ")[0]);
+    const b = data.map(value => +value.split("  ")[1]);
+
+    return a
+        .map((value) => b.filter(value1 => value1 == value).length * value)
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
 await runSolution(day1b);
