d64c252f9b012477a301ede3bb5c2931e0cda851
ampersanded <salvobonsma@gmail.com>
2024-12-01 22:15:49 -0800

    day 2 pls dont flame my code i was hella sick when i wrote it

--- day-2/a.ts
+++ day-2/a.ts
@@ -3,7 +3,22 @@ import {runSolution} from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
     console.log(data);
-    return 0;
+
+    return data.map(value => {
+        return value.split(" ")
+            .map(value1 => +value1)
+            .map((value1, index, array) => {
+                if (index == 0) return true;
+                return Math.abs(value1 - array[index - 1]) >= 1 && Math.abs(value1 - array[index - 1]) <= 3;
+            })
+            .find(value1 => value1 == false) == undefined && (
+            value.split(" ").map(value1 => +value1).map((value1, index, array) => {
+                return value1 > array[index - 1]
+            }).find(value1 => value1 == true) == undefined || value.split(" ").map(value1 => +value1).map((value1, index, array) => {
+                return value1 < array[index - 1]
+            }).find(value1 => value1 == true) == undefined
+        )
+    }).filter(value => value == true).length;
 }
 
 await runSolution(day2a);
--- day-2/b.ts
+++ day-2/b.ts
@@ -3,7 +3,30 @@ import {runSolution} from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day2b(data: string[]) {
     console.log(data);
-    return 0;
+
+    function isSafeFunc(row: number[]) {
+        return row
+            .map((value1, index, array) => {
+                if (index == 0) return true;
+                return Math.abs(value1 - array[index - 1]) >= 1 && Math.abs(value1 - array[index - 1]) <= 3;
+            })
+            .find(value1 => value1 == false) == undefined && (
+            row.map((value1, index, array) => {
+                return value1 > array[index - 1]
+            }).find(value1 => value1 == true) == undefined || row.map((value1, index, array) => {
+                return value1 < array[index - 1]
+            }).find(value1 => value1 == true) == undefined
+        )
+    }
+
+    return data.map(value => {
+        const isSafe = isSafeFunc(value.split(" ").map(value1 => +value1));
+        return isSafe ? true : value
+            .split(" ")
+            .map(value1 => +value1)
+            .map((_, index, array) => isSafeFunc(array.filter((_, index1) => index != index1)))
+            .find(value1 => value1 == true) != undefined
+    }).filter(value => value == true).length;
 }
 
 await runSolution(day2b);
