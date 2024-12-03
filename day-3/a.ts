ad68fc51ab0e9ca12db27556bef264ba1a7b686c
ampersanded <salvobonsma@gmail.com>
2024-12-02 21:39:45 -0800

    day 3

--- day-3/a.ts
+++ day-3/a.ts
@@ -3,7 +3,22 @@ import {runSolution} from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
     console.log(data);
-    return 0;
+
+    const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+
+    return data
+        .join("")
+        .match(regex)
+        .map(value => {
+            const values = value
+                .replace("mul(", "")
+                .replace(")", "")
+                .split(",")
+                .map(value1 => +value1);
+
+            return values[0] * values[1]
+        })
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -3,7 +3,32 @@ import {runSolution} from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day3b(data: string[]) {
     console.log(data);
-    return 0;
+    let string = data.join("");
+    const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+    const doDoNotRegex = /do\(\)|don't\(\)/g;
+    const muls = string.match(regex);
+
+    let canDo  = true;
+
+    return muls
+        .map(value => {
+            const index = string.indexOf(value);
+            const stringBefore =  string.slice(0, index);
+            string = string.slice(index + value.length, string.length);
+
+            const doDoNotBefore = stringBefore.match(doDoNotRegex);
+            if (doDoNotBefore?.length != null) canDo = doDoNotBefore[doDoNotBefore.length - 1] == "do()";
+            if (!canDo) return 0;
+
+            const values = value
+                .replace("mul(", "")
+                .replace(")", "")
+                .split(",")
+                .map(value1 => +value1);
+
+            return values[0] * values[1];
+        })
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
 }
 
 await runSolution(day3b);
