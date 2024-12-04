c76b346bdb31a65e0cb822b34192403f9247d4d8
ampersanded <salvobonsma@gmail.com>
2024-12-03 22:24:43 -0800

    day 4

--- day-4/a.ts
+++ day-4/a.ts
@@ -3,7 +3,57 @@ import {runSolution} from '../utils.ts';
 /** provide your solution as the return of this function */
 export async function day4a(data: string[]) {
     console.log(data);
-    return 0;
+    const twoD = data.map(value => value.split(""));
+    let total = 0;
+
+    twoD.forEach((row, y) => row.map((value, x) => {
+        if (value != "X") return;
+
+        function getCords(x2: number, y2: number) {
+            try {
+                return twoD[y + y2][x + x2];
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            } catch (e) {
+                return "";
+            }
+        }
+
+        function right() {
+            return getCords(1, 0) == "M" && getCords(2, 0) == "A" && getCords(3, 0) == "S" ? 1 : 0
+        }
+
+        function left() {
+            return getCords(-1, 0) == "M" && getCords(-2, 0) == "A" && getCords(-3, 0) == "S" ? 1 : 0
+        }
+
+        function up() {
+            return getCords(0, -1) == "M" && getCords(0, -2) == "A" && getCords(0, -3) == "S" ? 1 : 0
+        }
+
+        function down() {
+            return getCords(0, 1) == "M" && getCords(0, 2) == "A" && getCords(0, 3) == "S" ? 1 : 0
+        }
+
+        function upRight() {
+            return getCords(1, -1) == "M" && getCords(2, -2) == "A" && getCords(3, -3) == "S" ? 1 : 0
+        }
+
+        function upLeft() {
+            return getCords(-1, -1) == "M" && getCords(-2, -2) == "A" && getCords(-3, -3) == "S" ? 1 : 0
+        }
+
+        function downRight() {
+            return getCords(1, 1) == "M" && getCords(2, 2) == "A" && getCords(3, 3) == "S" ? 1 : 0
+        }
+
+        function downLeft() {
+            return getCords(-1, 1) == "M" && getCords(-2, 2) == "A" && getCords(-3, 3) == "S" ? 1 : 0
+        }
+
+        total += right() + left() + up() + down() + upRight() + downRight() + upLeft() + downLeft();
+    }));
+
+    return total;
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -2,8 +2,41 @@ import {runSolution} from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-    console.log(data);
-    return 0;
+    const twoD = data.map(value => value.split(""));
+    let total = 0;
+
+    twoD.forEach((row, y) => row.map((value, x) => {
+        if (value != "A") return;
+
+        function getCords(x2: number, y2: number) {
+            try {
+                return twoD[y + y2][x + x2];
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            } catch (e) {
+                return "";
+            }
+        }
+
+        function upRight() {
+            return getCords(-1, 1) == "M" && getCords(1, -1) == "S" ? 1 : 0
+        }
+
+        function downLeft() {
+            return getCords(-1, 1) == "S" && getCords(1, -1) == "M" ? 1 : 0
+        }
+
+        function downRight() {
+            return getCords(-1, -1) == "M" && getCords(1, 1) == "S" ? 1 : 0
+        }
+
+        function upLeft() {
+            return getCords(-1, -1) == "S" && getCords(1, 1) == "M" ? 1 : 0
+        }
+
+        total += upRight() + downLeft() + downRight() + upLeft() == 2 ? 1 : 0;
+    }));
+
+    return total;
 }
 
 await runSolution(day4b);
